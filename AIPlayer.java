cf777f1f28a9e4214c73b5d766d8d1e1eece4a1f
eudus <eudusb@shaw.ca>
2024-03-01 12:10:41 -0800

    Added documentation to HumanPlayer and added base of AIPlayer class

---
+++ AIPlayer.java
@@ -0,0 +1,30 @@
+/**
+ * This file is part of a solution to the CSPC 101 Group project.
+ * 
+ * @author Logan White
+ * Student Number: 230161077
+ * @version 1
+ */
+package scoreFour;
+
+/**
+ * A class representing the AI player.
+ */
+class AIPlayer {
+	private Board mainBoard;
+	/**
+	 * Creates a new AI player which will play on the provided board
+	 
+	 * @param mainBoard The board the AIPlayer will play on
+	 */
+	public AIPlayer(Board mainBoard) {
+		this.mainBoard = mainBoard;
+	}
+
+	/**
+	 * Plays a move at a board position determined by the AI
+	 */
+	public void playNextMove() {
+
+	}
+}
--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,11 +1,32 @@
+/**
+ * This file is part of a solution to the CSPC 101 Group project.
+ * 
+ * @author Logan White
+ * Student Number: 230161077
+ * @version 1
+ */
 package scoreFour;
 
+/**
+ * A class representing the human player.
+ */
 class HumanPlayer {
 	private Board mainBoard;
+	/**
+	 * Creates a new human player which will play on the provided board
+	 *
+	 * @param mainBoard The board the HumanPlayer will play on
+	 */
 	HumanPlayer(Board mainBoard) {
 		this.mainBoard = mainBoard;
 	}
 
+	/**
+	 * Plays a move at boardPosition (x, y)
+	 *
+	 * @param x The x-pos of the move
+	 * @param y The y-pos of the move
+	 */
 	public void playNextMove(int x, int y) {
 		mainBoard.putBeadAt(x, y);
 	}
