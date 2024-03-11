87ef2036746893b9927c5e3456b51deeef39df5a
eudus <eudusb@shaw.ca>
2024-03-11 14:46:55 -0700

    Added Main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ * This file is part of a solution to the CSPC 101 Group project.
+ * 
+ * @author Logan White
+ * Student Number: 230161077
+ * @version 1
+ */
+package scoreFour;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
